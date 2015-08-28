bb77bae588f7caac220cbee63edd599c4b71eb89
Wolfgang Wiese <xwolf@xwolf.de>
2015-08-28 17:08:46 +0200

    Id optional anzeigen lassen bei Seiten https://github.com/RRZE-Webteam/FAU-Medfak/issues/15

--- functions/constants.php
+++ functions/constants.php
@@ -208,6 +208,7 @@ $defaultoptions = array(
     'index_synonym_listall'		=> true,
     'index_glossary_listall'		=> true,
     
+    'advanced_reveal_pages_id'		=> true,
 
      
 ); 
@@ -872,6 +873,14 @@ $setoptions = array(
                   'default' => $defaultoptions['advanced_post_active_subtitle'],
 		  'parent'  => 'bedienung'
               ),   
+	       
+	       	'advanced_reveal_pages_id'	=> array(
+                  'type'    => 'bool',
+                  'title'   => __( 'Zeige Seiten-Ids', 'fau' ),
+                  'label'   => __( 'In der Übersicht der Seiten werden die Ids angezeigt.', 'fau' ),                
+                  'default' => $defaultoptions['advanced_reveal_pages_id'],
+		  'parent'  => 'bedienung'
+                ),
 	       
 	       
 	      'design'  => array(
