12c2a607ecccc53dd85a302b6b9229a6edbe9ba3
Wolfgang Wiese <xwolf@xwolf.de>
2015-09-15 17:54:39 +0200

    Copyright Info Bugfix wpLink-Settings Bugfix IE Intranet Copyright-Options im Backend Standardisierte CSS-Klassen für Absätze Bild-Anzeige im Attachment

--- page-templates/page-start.php
+++ page-templates/page-start.php
@@ -70,7 +70,7 @@ global $options;
 				$slidersrc = '<img src="'.fau_esc_url($sliderimage[0]).'" width="'.$options['slider-image-width'].'" height="'.$options['slider-image-height'].'" alt="">';	
 			    }
 			    echo $slidersrc."\n"; 
-			    if (!empty($copyright)) {
+			    if (($options['advanced_display_hero_credits']==true) && (!empty($copyright))) {
 				echo '<p class="credits">'.$copyright."</p>";
 			    }
 			    ?>
